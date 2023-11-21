e92daa2afeea413e95d7683a2f4de2e2a113d388
H8YOU212 <slavyan.212@mail.ru>
2023-11-21 11:20:04 +0500

    +database

---
+++ Models/Cpu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompCompany1.Models;
+
+public partial class Cpu
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Price { get; set; }
+
+    public string ProducingCountry { get; set; } = null!;
+}
