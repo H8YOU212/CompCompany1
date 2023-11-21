dd49e32e65506733e8eee90cd75c0f7e22a90b00
H8YOU212 <slavyan.212@mail.ru>
2023-11-21 11:22:34 +0500

    CpuController

---
+++ Controllers/CpuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompCompany1.Controllers
+{
+    public class CpuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
