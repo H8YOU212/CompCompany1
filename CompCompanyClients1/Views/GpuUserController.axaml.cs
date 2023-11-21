e1174ea1d80594726cf1857455ca88af22569139
wdfwwkkee <denis.usmanov101@mail.ru>
2023-11-21 12:48:44 +0500

    GpuUserControllerView

---
+++ CompCompanyClients1/Views/GpuUserController.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CompCompanyClients1.Views;
+
+public partial class GpuUserController : UserControl
+{
+    public GpuUserController()
+    {
+        InitializeComponent();
+    }
+}
