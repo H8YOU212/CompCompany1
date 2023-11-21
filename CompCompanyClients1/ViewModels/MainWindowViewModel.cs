7f08bcde37caf1dd89b8ce367fddff14b4519b90
H8YOU212 <slavyan.212@mail.ru>
2023-11-21 13:34:18 +0500

    micro-fix

--- CompCompanyClients1/ViewModels/MainWindowViewModel.cs
+++ CompCompanyClients1/ViewModels/MainWindowViewModel.cs
@@ -7,7 +7,12 @@ namespace CompCompanyClients1.ViewModels
         {
             _CpuUserController = new CpuUserController();
             _CpuUserController.DataContext = new CpuUserControllerViewModel();
+
+            _GpuUserController = new GpuUserController();
+            _GpuUserController.DataContext = new GpuUserControllerViewModel();
         }
         public CpuUserController _CpuUserController { get; set; }
+        public GpuUserController _GpuUserController { get; set; }
+
     }
 }
